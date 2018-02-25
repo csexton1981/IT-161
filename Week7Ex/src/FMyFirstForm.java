c927743f258f222f4d6744facf75c7738b132b6a
ChrisSexton <ChrisSexton@192.168.200.229>
2018-02-25 11:15:22 -0500

    Week7 Homework Initial Commit

---
+++ Week7Ex/src/FMyFirstForm.java
@@ -0,0 +1,58 @@
+// ----------------------------------------------------------------------
+// Name: Christopher Sexton
+// Date: 2/21/18
+// Class Name: 
+// Abstract: the starting form for our windows application
+// ----------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------
+// Imports
+// ----------------------------------------------------------------------
+//this imports JButton, JFrame
+
+import javax.swing.*;
+
+
+@SuppressWarnings("serial")
+public class FMyFirstForm extends JFrame
+{
+	// ----------------------------------------------------------------------
+	// Name: FMain
+	// Abstract: the default constructor
+	// ----------------------------------------------------------------------
+
+		public FMyFirstForm( )
+		{
+		
+		int intHeight = 500;
+		int intWidth = 500;
+
+		setTitle ("This class is tougher than I thought");
+		
+		setSize( intWidth, intHeight );  
+		
+		setResizable( false );
+		
+		this.setLocationRelativeTo( null );
+		
+		// Very important or we get zombies (undead application)
+		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+
+		// Clear layout manager so we can manually size and position controls
+		// comment this line out and test your program - you will see a large button
+				
+		// Create instance
+		JButton btnTest = new JButton( "Test Chris' Button" );
+
+		// Add to form
+		this.add(btnTest);
+		
+		// Set position and size (left, top, width, height)
+		btnTest.setBounds( 100, 100, 200, 35 );
+		
+		this.setLayout( null );  // Death to layout manager overlords!
+		
+	}
+	
+}
---
+++ Week7Ex/src/Week7IC1.java
@@ -0,0 +1,29 @@
+// Name: Christopher Sexton
+// Date: 2/21/18
+// Abstract: Week 7 InClass 1
+
+public class Week7IC1 {
+
+	public static void main(String[] args)
+	{
+		try 
+		{
+			// FMain #1
+			FMyFirstForm frmMain1 = new FMyFirstForm( );
+			// Put this code in for the class to form an error
+			
+			//int intTest = 0;
+			//intTest = 1/0;
+			//System.out.println( intTest * 1 );			
+			frmMain1.setVisible( true );
+			//FMyFirstForm frmMain1
+			
+		}
+		catch ( Exception excError )
+		{
+			// Display Error Message
+			System.out.println( excError.getMessage());
+		}
+	}
+
+}
