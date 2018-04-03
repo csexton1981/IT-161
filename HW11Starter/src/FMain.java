e6eb176d25b20024efa74f1b98ad3f8979fee505
csexton1981 <csexton1981@gmail.com>
2018-04-02 21:56:09 -0400

    Final and tested good

--- HW11Starter/src/FMain.java
+++ HW11Starter/src/FMain.java
@@ -14,7 +14,7 @@ import java.awt.*;						// Basic windows functionality
 import java.awt.event.*;				// Event processing
 import javax.swing.*;					// Supplemental windows functionality
 import Utilities.*;
-import java.sql.*;						// All things SQL
+// import java.sql.*;						// All things SQL
 
 
 @SuppressWarnings({ "serial", "unused" })
@@ -38,7 +38,7 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 	// ----------------------------------------------------------------------
 	// Never make public properties.  
 	// Make protected or private with public get/set methods
-	private Connection m_conAdministrator = null;
+	// private Connection m_conAdministrator = null;
 	private CListBox m_lstTeams = null;
 	
 	// ----------------------------------------------------------------------
@@ -118,13 +118,13 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 			this.setLayout( null );
 	
 			// Open
-			m_btnOpenDatabaseConnection 	= CUtilities.AddButton( this, this, "Open Database Connection",   20, 20,  35, 250 );
+			// m_btnOpenDatabaseConnection 	= CUtilities.AddButton( this, this, "Open Database Connection",   20, 20,  35, 250 );
 			// Load Teams Button
 			m_btnLoadTeamsList			 	= CUtilities.AddButton( this, this, "Load Teams List",   65, 20,  35, 250 );
 			// Teams List
 			m_lstTeams						= CUtilities.AddListBox( this,                                   110, 20, 280, 250 );
 			// Close
-			m_btnCloseDatabaseConnection 	= CUtilities.AddButton( this, this, "Close Database Connection",   400, 20,  35, 250 );
+			// m_btnCloseDatabaseConnection 	= CUtilities.AddButton( this, this, "Close Database Connection",   400, 20,  35, 250 );
 			
 		}
 		catch( Exception excError )
@@ -137,6 +137,12 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 	// ----------------------------------------------------------------------
 	// Name: WindowListener
 	// ----------------------------------------------------------------------	
+	
+	// Name: Window Opened
+	// Purpose: Opens a connection to the database.  Since the form is visible now, we need to ensure that a connection
+	// is opened and setting the busy cursor while you're connecting to the database so the user knows something is going on
+	// under the covers.  
+	
 	public void windowOpened( WindowEvent weSource )
 	{
 		try
@@ -145,7 +151,7 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 			CUtilities.SetBusyCursor( this, true );
 
 			// Can we connect to the database?
-			if( CDatabaseUtilities.OpenDatabaseConnectionMSAccessJRE8( ) == false )
+			if( CDatabaseUtilities.OpenDatabaseConnection( ) == false )
 			{
 				
 				// No, warn the user ...
@@ -233,7 +239,8 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 	public void windowDeactivated( WindowEvent weSource ) { }
 	public void windowIconified( WindowEvent weSource ) { }
 	public void windowDeiconified( WindowEvent weSource ) { }	
-	
+
+
 
 	// ----------------------------------------------------------------------
 	// Name: actionPerformed
@@ -246,11 +253,11 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 		{
 			// VB.Net Event Procedure Names: <Control Name>_<Event Type>
 			
-				 if( aeSource.getSource( ) == m_btnOpenDatabaseConnection ) 	btnOpenDatabaseConnection2_Click( );
+				 //if( aeSource.getSource( ) == m_btnOpenDatabaseConnection ) 	btnOpenDatabaseConnection2_Click( );
 				 
 				 if ( aeSource.getSource( ) == m_btnLoadTeamsList ) btnLoadTeamsList_Click( );
 					
-				 if( aeSource.getSource( ) == m_btnCloseDatabaseConnection ) 	btnCloseDatabaseConnection_Click( );
+				 //if( aeSource.getSource( ) == m_btnCloseDatabaseConnection ) 	btnCloseDatabaseConnection_Click( );
 		}
 		catch( Exception excError )
 		{
@@ -259,57 +266,8 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 		}
 	}
 	
-	// ----------------------------------------------------------------------
-	// Name: btnOpenDatabaseConnection2_Click
-	// Abstract: Open a connection to the database
-	//			The support for the JDBC-ODBC bridge has been removed from
-	//			JDK1.8 and JRE8.
-	//
-	//			Use UCanAccess, an open-source JDBC driver, instead.
-	//			Include the following jar files in your code:
-	//				ucanaccess-2.0.7.jar
-	//				jackcess-2.0.4.jar
-	//					commons-lang-2.6.jar
-	//					commons-logging-1.1.3.jar
-	//				hsqldb.jar
-	//
-	//			To include those files select "Project / Properties / Java Build Path"
-	//			from the menu.  Click on the "Libraries" tab.  Click "Add External JARs".
-	//			Browse to the above jar files, which should be in a directory in your
-	//			project (e.g. JDBC-to-MSAccess).  Select all five files and click
-	//			"Open".  Click "OK".
-	//
-	//			You must also have either have MS Access 2010 install or download and
-	//			install "Microsoft Access Database Engine 2010" which can be found 
-	//			for free at:
-	//
-	//				https://www.microsoft.com/en-us/download/details.aspx?id=13255
-	//
-	//			Do not match to your operating system.  Download the version 
-	//			(32 or 64 bit) that matches the version of your JRE.  
-	// ----------------------------------------------------------------------
-	public void btnOpenDatabaseConnection2_Click( )
-	{
-		try
-		{
-			String strConnectionString = "";
 
-			// Server name/port, IP address/port or path for file based databases like MS Access
-			// System.getProperty( "user.dir" ) => Current working directory from where
-			// application was started
-			strConnectionString = "jdbc:ucanaccess://" + System.getProperty( "user.dir" ) 
-								+ "\\Database\\TeamsAndPlayers1.accdb";
 
-			// Open a connection to the database
-			m_conAdministrator = DriverManager.getConnection( strConnectionString );
-		}
-		catch( Exception excError )
-		{
-			// Display Error Message
-			CUtilities.WriteLog( excError );
-		}
-	}
-	
 	// ----------------------------------------------------------------------
 	// Name: btnLoadTeamsList_OnClick
 	// Abstract: Load the team list
@@ -318,36 +276,7 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 	{
 		try
 		{
-			String strSelect = "";
-			Statement sqlCommand = null;
-			ResultSet rstTTeams = null;
-			String strTeam = "";
-			
-			// Clear list to avoid duplicate entry
-			m_lstTeams.Clear( );
-			
-			// Build the SQL string
-			strSelect = "SELECT strTeam" 
-					  + " FROM TTeams" 
-					  + " ORDER BY strTeam";
-
-			// Retrieve the all the records
-			sqlCommand = m_conAdministrator.createStatement( );
-			rstTTeams = sqlCommand.executeQuery( strSelect );
-
-			// Loop through all the records
-			while( rstTTeams.next( ) == true )
-			{
-				// Get Name from current row, which should be the first, and only, column
-				strTeam = rstTTeams.getString( 1 );
-
-				// Add the item to the list (0 = id, strTeam = text to display, false = don't select)
-				m_lstTeams.AddItemToList( 0, strTeam, false );
-			}
-			
-			// Clean up
-			rstTTeams.close( );
-			sqlCommand.close( );
+			CDatabaseUtilities.LoadListBoxFromDatabase( "TTeams", "strTeam", m_lstTeams );
 		}
 		catch( Exception excError )
 		{
@@ -355,37 +284,5 @@ public class FMain extends JFrame implements ActionListener, WindowListener
 			CUtilities.WriteLog( excError );
 		}
 	}
-
-	
-	// ----------------------------------------------------------------------
-	// Name: btnCloseDatabaseConnection_Click
-	// Abstract: Close the connection to the database
-	// ----------------------------------------------------------------------
-	public void btnCloseDatabaseConnection_Click( )
-	{
-		try
-		{
-			// Is there a connection object?
-			if( m_conAdministrator != null )
-			{
-				// Yes, close the connection if not closed already
-				if( m_conAdministrator.isClosed( ) == false ) 
-				{
-					// Clear list to avoid duplicate entry
-					//m_lstTeams.Clear( );
-					m_conAdministrator.close( );
-					m_lstTeams.Clear( );
-					
-					// Prevent JVM from crashing
-					m_conAdministrator = null;
-				}
-			}
-		}
-		catch( Exception excError )
-		{
-			// Display Error Message
-			CUtilities.WriteLog( excError );
-		}
-	}
-	
 }
+	
